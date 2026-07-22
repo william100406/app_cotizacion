ecd7c91b570ef69ecdf5db43bb4066e4817d47a4
William Del Valle <williamomardelvalle123@gmail.com>
2026-07-22 09:56:26 -0400

    no se qu eera

--- app.py
+++ app.py
@@ -2163,5 +2163,4 @@ if __name__ == "__main__":
         except Exception as e:
             print("Error arreglando tabla:", e)
 
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
